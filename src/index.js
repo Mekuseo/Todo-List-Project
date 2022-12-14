eb5ae147b861dc9b894cde40d6be2068509e5eb8
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-12-14 14:58:31 +0100

    add-remove-edit task completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-/* eslint-disable import/no-cycle */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-mutable-exports */
-
 import './style.css';
-import myTodo from './modules/functionality.js';
+import addEditRemoveTask from './modules/functionality.js';
 
-const todos = document.querySelector('.todos');
+window.addEventListener('load', () => {
+  const form = document.querySelector('#new-task-form');
 
-const todoList = myTodo.map((todo) => `<li class="todo-item">
-    <input type="checkbox" id="todo-${todo.index}" />
-    <label for="todo-${todo.index}">${todo.description}</label>
-  </li>`).join('');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-todos.innerHTML = todoList;
+    // this function adds the new task and returns the task container
+    addEditRemoveTask();
+  });
+});
--- src/modules/functionality.js
+++ src/modules/functionality.js
@@ -1,49 +1,85 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/prefer-default-export */
+const input = document.querySelector('#new-task-input');
+const listContainer = document.querySelector('#tasks');
+const errorMessage = document.querySelector('.error-message');
 
-export const myTodo = [];
+export default function addEditRemoveTask() {
+  const task = input.value;
 
-// function for adding a new task to array
-const addTask = () => {
-  const input = document.querySelector('.input');
-  const newTask = input.value;
-  const todo = { description: newTask, completed: false, index: myTodo.length };
-  myTodo.push(todo);
+  // function for adding new task to the list
+  const taskContainer = document.createElement('div');
+  taskContainer.classList.add('task');
+
+  const taskContentContainer = document.createElement('div');
+  taskContentContainer.classList.add('content');
+
+  taskContainer.appendChild(taskContentContainer);
+
+  const taskCheckboxContainer = document.createElement('input');
+  taskCheckboxContainer.classList.add('checkbox');
+  taskCheckboxContainer.type = 'checkbox';
+
+  const taskInputContainer = document.createElement('input');
+  taskInputContainer.classList.add('text');
+  taskInputContainer.type = 'text';
+  taskInputContainer.value = task;
+  taskInputContainer.setAttribute('readonly', 'readonly');
+
+  taskContentContainer.appendChild(taskCheckboxContainer);
+  taskContentContainer.appendChild(taskInputContainer);
+
+  // function for editing and removing task from the list
+  const taskActionsContainer = document.createElement('div');
+  taskActionsContainer.classList.add('actions');
+
+  const taskEditButton = document.createElement('button');
+  taskEditButton.classList.add('edit');
+  taskEditButton.innerText = 'Edit';
+
+  const taskDeleteButton = document.createElement('button');
+  taskDeleteButton.classList.add('delete');
+  taskDeleteButton.innerText = 'Delete';
+
+  taskActionsContainer.appendChild(taskEditButton);
+  taskActionsContainer.appendChild(taskDeleteButton);
+
+  taskContainer.appendChild(taskActionsContainer);
+
+  listContainer.appendChild(taskContainer);
+
   input.value = '';
-};
 
-// function for deleting a task from array
-const deleteTask = (index) => {
-  myTodo.splice(index, 1);
-};
-
-// function for updating a task description
-const updateTask = (index, description) => {
-  myTodo[index].description = description;
-};
-
-// function for updating an item's 'completed' status
-const completedTask = (index) => {
-  myTodo[index].completed = !myTodo[index].completed;
-};
-
-// function for updating an item's index value
-const updateIndex = () => {
-  myTodo.forEach((todo, index) => {
-    todo.index = index + 1;
-  });
-};
-
-// function for clearing all completed tasks
-const clearCompleted = () => {
-  myTodo.forEach((todo, index) => {
-    if (todo.completed) {
-      myTodo.splice(index, 1);
+  taskEditButton.addEventListener('click', () => {
+    if (taskEditButton.innerText.toLowerCase() === 'edit') {
+      taskEditButton.innerText = 'Save';
+      taskInputContainer.removeAttribute('readonly');
+      taskInputContainer.focus();
+    } else {
+      taskEditButton.innerText = 'Edit';
+      taskInputContainer.setAttribute('readonly', 'readonly');
     }
   });
-};
 
-// function for updating the array values on local storage
-const updateStorage = () => {
-  localStorage.setItem('myTodo', JSON.stringify(myTodo));
-};
+  // function for removing task from the list
+  taskDeleteButton.addEventListener('click', () => {
+    listContainer.removeChild(taskContainer);
+  });
+
+  // delete all tasks from the list
+  const deleteAllButton = document.querySelector('.delete-all');
+  deleteAllButton.addEventListener('click', () => {
+    listContainer.innerHTML = '';
+  });
+
+  // Error message for no task input
+   if (task === '') {
+    taskContainer.remove();
+    const error = document.createElement('div');
+    error.classList.add('error');
+    error.innerText = 'Please enter a task';
+    errorMessage.appendChild(error);
+
+    setTimeout(() => {
+      error.remove();
+    }, 2000);
+  }
+}
