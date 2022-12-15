1d1a01b59286e6c0ef133cee6f11816c1ddbe490
Chukwuemeka Kanu <mekuseo@gmail.com>
2022-12-15 15:31:47 +0100

    add and remove changes complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import './style.css';
-import addEditRemoveTask from './modules/functionality.js';
+import { formSubmission, populateTasks } from './modules/functionality.js';
 
-window.addEventListener('load', () => {
-  const form = document.querySelector('#new-task-form');
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    // this function adds the new task and returns the task container
-    addEditRemoveTask();
-  });
-});
+populateTasks();
+formSubmission();
--- src/modules/functionality.js
+++ src/modules/functionality.js
@@ -1,75 +1,63 @@
+/* eslint-disable array-callback-return */
+
 const input = document.querySelector('#new-task-input');
 const listContainer = document.querySelector('#tasks');
 const errorMessage = document.querySelector('.error-message');
-
-export default function addEditRemoveTask() {
-  const task = input.value;
-
+const form = document.getElementById('new-task-form');
+let taskStorage = [];
+let taskObject;
+const saveTodo = () => {
+  const allTodos = JSON.stringify(taskStorage);
+  localStorage.setItem('taskStorage', allTodos);
+};
+const getStoredTodos = () => {
+  taskStorage = JSON.parse(localStorage.getItem('taskStorage'));
+};
+const createTask = () => {
+  taskObject = {
+    description: input.value,
+    completed: false,
+    index: taskStorage.length + 1,
+  };
+  if (taskObject.description !== '') {
+    taskStorage.push(taskObject);
+    saveTodo();
+  }
+  // taskStorage.push(taskObject);
+  // saveTodo();
+};
+export default function addEditRemoveTask(task) {
   // function for adding new task to the list
   const taskContainer = document.createElement('div');
   taskContainer.classList.add('task');
-
   const taskContentContainer = document.createElement('div');
   taskContentContainer.classList.add('content');
-
   taskContainer.appendChild(taskContentContainer);
-
   const taskCheckboxContainer = document.createElement('input');
   taskCheckboxContainer.classList.add('checkbox');
   taskCheckboxContainer.type = 'checkbox';
-
   const taskInputContainer = document.createElement('input');
   taskInputContainer.classList.add('text');
   taskInputContainer.type = 'text';
-  taskInputContainer.value = task;
+  taskInputContainer.value = task.description;
   taskInputContainer.setAttribute('readonly', 'readonly');
-
   taskContentContainer.appendChild(taskCheckboxContainer);
   taskContentContainer.appendChild(taskInputContainer);
-
-  // creating a local storage
-  const taskLists = [];
-  const taskObject = [
-    {
-      description: task,
-      completed: false,
-      index: taskLists.length + 1,
-    },
-  ];
-  taskLists.push(taskObject);
-
-  for (let i = 0; i < taskLists.length; i += 1) {
-    taskLists[i].index = i + 1;
-    let taskStorage;
-    if (localStorage.getItem('taskStorage') === null) {
-      taskStorage = [];
-    } else {
-      taskStorage = JSON.parse(localStorage.getItem('taskStorage'));
-    }
-    taskStorage.push(taskLists);
-    localStorage.setItem('taskStorage', JSON.stringify(taskStorage));
-  }
 
   // function for editing and removing task from the list
   const taskActionsContainer = document.createElement('div');
   taskActionsContainer.classList.add('actions');
-
   const taskEditButton = document.createElement('button');
   taskEditButton.classList.add('edit');
   taskEditButton.innerText = 'Edit';
-
   const taskDeleteButton = document.createElement('button');
   taskDeleteButton.classList.add('delete');
   taskDeleteButton.innerText = 'Delete';
-
   taskActionsContainer.appendChild(taskEditButton);
   taskActionsContainer.appendChild(taskDeleteButton);
-
   taskContainer.appendChild(taskActionsContainer);
-
   listContainer.appendChild(taskContainer);
   input.value = '';
-
   taskEditButton.addEventListener('click', () => {
     if (taskEditButton.innerText.toLowerCase() === 'edit') {
       taskEditButton.innerText = 'Save';
@@ -93,15 +81,37 @@ export default function addEditRemoveTask() {
   });
 
   // Error message for no task input
-  if (task === '') {
+  if (task.description === '') {
     taskContainer.remove();
     const error = document.createElement('div');
     error.classList.add('error');
     error.innerText = 'Please enter a task';
     errorMessage.appendChild(error);
-
     setTimeout(() => {
       error.remove();
     }, 2000);
   }
 }
+
+function formSubmission() {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createTask();
+    addEditRemoveTask(taskObject);
+  });
+}
+
+const populateTasks = () => {
+  if (localStorage.getItem('taskStorage') !== null) {
+    getStoredTodos();
+    taskStorage.map((task) => {
+      addEditRemoveTask(task);
+    });
+  } else {
+    const taskInputContainer = document.createElement('h3');
+    taskInputContainer.classList.add('text');
+    taskInputContainer.textContent = 'No available tasks!!';
+    listContainer.appendChild(taskInputContainer);
+  }
+};
+export { populateTasks, formSubmission };
